00aa7be0e83e7a187e16b7ed58a6ace368761437
JeanVanneste <jean.vanneste@student.vinci.be>
2019-05-29 15:24:42 +0200

    renaming files

---
+++ src/app/editeur.ts
@@ -0,0 +1,7 @@
+export class Editeur {
+    id: number;
+    name: string;
+    nationalite: string;
+    creationYear: number;
+  }
+  
---
+++ src/app/jeu.ts
@@ -0,0 +1,12 @@
+export class Jeu {
+    id: number;
+    editeurId: number;
+    name: string;
+    author: string;
+    category: string;
+    gameDuration: number;
+    playerMin: number;
+    playerMax: number;
+    description: string;
+  }
+  
